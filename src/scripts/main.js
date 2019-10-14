be0ee8c2fe6836e41e35ef460581fc7ade5bc4e9
Ryan Bishop <rdbishop06@gmail.com>
2019-10-14 11:48:56 -0500

    Refactor helper function into generic html element generator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,78 +1,78 @@
-console.log("HERE WE IS!");
+console.log('HERE WE IS!');
 const students = [
-    {
-        name: "Chris Miller",
-        subject: "History",
-        info: "Failed last exam",
-        score: 59
-    },
-    {
-        name: "Courtney Seward",
-        subject: "History",
-        info: "Has completed all homework",
-        score: 91
-    },
-    {
-        name: "Garrett Ward",
-        subject: "History",
-        info: "Wonderful at helping other students",
-        score: 88
-    },
-    {
-        name: "John Dulaney",
-        subject: "History",
-        info: "Has never missed a class or exam",
-        score: 92
-    },
-    {
-        name: "Greg Lawrence",
-        subject: "History",
-        info: "Sub-par performance all around",
-        score: 64
-    },
-    {
-        name: "Leah Duvic",
-        subject: "History",
-        info: "Wonderful student",
-        score: 97
-    },
-    {
-        name: "Jesse Page",
-        subject: "History",
-        info: "Smokes too much. Distracting.",
-        score: 76
-    },
-    {
-        name: "Kevin Haggerty",
-        subject: "History",
-        info: "Falls asleep in class",
-        score: 79
-    },
-    {
-        name: "Max Wolf",
-        subject: "History",
-        info: "Talks too much",
-        score: 83
-    },
-    {
-        name: "Lissa Goforth",
-        subject: "History",
-        info: "Asks pointless, unrelated questions",
-        score: 78
-    },
-    {
-        name: "Tyler Bowman",
-        subject: "History",
-        info: "When was the last time he attended class?",
-        score: 48
-    },
-    {
-        name: "Ray Medrano",
-        subject: "History",
-        info: "Needs to contribute to in-class discussions",
-        score: 95
-    }
-]
+	{
+		name: 'Chris Miller',
+		subject: 'History',
+		info: 'Failed last exam',
+		score: 59
+	},
+	{
+		name: 'Courtney Seward',
+		subject: 'History',
+		info: 'Has completed all homework',
+		score: 91
+	},
+	{
+		name: 'Garrett Ward',
+		subject: 'History',
+		info: 'Wonderful at helping other students',
+		score: 88
+	},
+	{
+		name: 'John Dulaney',
+		subject: 'History',
+		info: 'Has never missed a class or exam',
+		score: 92
+	},
+	{
+		name: 'Greg Lawrence',
+		subject: 'History',
+		info: 'Sub-par performance all around',
+		score: 64
+	},
+	{
+		name: 'Leah Duvic',
+		subject: 'History',
+		info: 'Wonderful student',
+		score: 97
+	},
+	{
+		name: 'Jesse Page',
+		subject: 'History',
+		info: 'Smokes too much. Distracting.',
+		score: 76
+	},
+	{
+		name: 'Kevin Haggerty',
+		subject: 'History',
+		info: 'Falls asleep in class',
+		score: 79
+	},
+	{
+		name: 'Max Wolf',
+		subject: 'History',
+		info: 'Talks too much',
+		score: 83
+	},
+	{
+		name: 'Lissa Goforth',
+		subject: 'History',
+		info: 'Asks pointless, unrelated questions',
+		score: 78
+	},
+	{
+		name: 'Tyler Bowman',
+		subject: 'History',
+		info: 'When was the last time he attended class?',
+		score: 48
+	},
+	{
+		name: 'Ray Medrano',
+		subject: 'History',
+		info: 'Needs to contribute to in-class discussions',
+		score: 95
+	}
+];
 
 /* 
 Instead of defining four arguments for the createStudentComponent function, 
@@ -82,46 +82,34 @@ refactor the function to accept the entire object as a single argument.
 Then refactor your string interpolation code to use the object properties.
 */
 const createStudentComponent = (student) => {
-    let studentComponent = "";
-    if (student.score >= 60) {
-        studentComponent = "passing"
-    } else {
-        studentComponent = "failing"
-    }
-    /* 
-    Write functions that build the sub-components of the larger student component.
+	let studentComponent = '';
+	if (student.score >= 60) {
+		studentComponent = 'passing';
+	} else {
+		studentComponent = 'failing';
+	}
+	/* 
+    Create one function that will generate any HTML component, with any content. 
+    It should be defined with three arguments.
     
-    h1
-    section
-    aside
-    
-    const createStudentComponent = (student) => `
-        <div id="student">
-            ${h1(student.name)}
-            ${section(student.subject)}
-            ${aside(student.info)}
-        </div>
-    `
-     */
-    function h1(name){
-        return `<h1 class="${studentComponent}">${student.name}</h1>`
-    }
-    function section(subject){
-        return `<section>${subject}</section>`
-    }
-    function aside(info){
-        return `<aside>${info}</aside>`
-    }
-    return `
-        <div class="student">
-            ${h1(student.name)}
-            ${section(student.subject)}
-            ${aside(student.info)}
-        </div>
-    `
-}
+    The type of HTML component to make
+    The content of the component
+    The value of the class attribute
 
-let studentContainer = document.querySelector("#container")
+    */
+   function element(tag, content, classList) {
+       return `<${tag} class="${classList}">${content}</${tag}>`;
+   }
+   return `
+        <div class="student">
+            ${element("h1", student.name, `xx-large ${studentComponent}`)}
+            ${element("section", student.subject, "bordered dashed section--padded")}
+            ${element("aside", student.info, "pushRight")}
+        </div>
+    `;
+};
+
+let studentContainer = document.querySelector('#container');
 
 // ## looping way (refactored with code below)
 // for (let i = 0; i < students.length; i++) {
@@ -130,6 +118,6 @@ let studentContainer = document.querySelector("#container")
 // }
 
 // ## forEach way
-students.forEach((student) =>{
-    studentContainer.innerHTML += createStudentComponent(student);
-})
+students.forEach((student) => {
+	studentContainer.innerHTML += createStudentComponent(student);
+});
